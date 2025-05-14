63e3fe5cab8c3bc0b0c4d82e46ca61fe0c09bbb9
Ismail Lyamani <ismaillyamani4@gmail.com>
2025-05-14 22:56:15 +0100

    driver

--- core/src/main/java/io/github/lo3ba/DAO/ConnexionBD.java
+++ core/src/main/java/io/github/lo3ba/DAO/ConnexionBD.java
@@ -15,13 +15,9 @@ public class ConnexionBD {
         Statement stm = null;
         try
         {
-            //1 charger le driver
             Class.forName("com.mysql.jdbc.Driver");
-            // établir la connexion avec la BDD
             connexion= DriverManager.getConnection(url,user, password);
-            // créer un espace d'exécution des requêtes
             stm = connexion.createStatement();
-
         }
         catch (Exception e)
         {
